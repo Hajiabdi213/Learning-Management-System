be0e22ce2271bf76cc74e18cad96e97df1ab760b
Abdillahi Haji <hajiabdi213@gmail.com>
2023-10-27 18:09:41 +0300

    Admin Registration is done

--- api/server.js
+++ api/server.js
@@ -1,11 +1,14 @@
 import express, { json } from "express";
-const server = express();
 import dotenv from "dotenv";
-dotenv.config();
+import userRouter from "./users.js";
+const server = express();
 
+dotenv.config();
 server.use(json());
 server.get("/", (req, res) => {
   res.status(200).json({ message: "Hello from server" });
 });
+
+server.use("/api/user", userRouter);
 
 export default server;
---
+++ api/users.js
@@ -0,0 +1,47 @@
+import prisma from "./lib/index.js";
+import express from "express";
+
+import bcryptjs from "bcryptjs";
+
+const userRouter = express.Router();
+
+// admin signup
+userRouter.post("/admin/signup", async (req, res) => {
+  const { firstName, lastName, image, email, password, phone } = req.body;
+
+  try {
+    // check if the user already exist or not
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (!user) {
+      // hash the password
+      const hashedPassword = await bcryptjs.hash(password, 10);
+      const admin = await prisma.user.create({
+        data: {
+          firstName,
+          lastName,
+          image,
+          email,
+          password: hashedPassword,
+          phone,
+          role: "admin",
+        },
+      });
+
+      res
+        .status(200)
+        .json({ message: "User created successfully", user: admin });
+    } else {
+      res
+        .status(400)
+        .json({ message: `user with the email ${email} already exists` });
+    }
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+});
+
+// instructor signup
+
+// student signup
+
+export default userRouter;
