387434c3ac485db31775c88f4a8a7628b3e8d4fc
Abdillahi Haji <hajiabdi213@gmail.com>
2023-11-05 10:54:26 +0300

    Assessment Model is added

--- api/middleware/roleAuth.js
+++ api/middleware/roleAuth.js
@@ -66,3 +66,29 @@ export const isAdminOrCourseCreator = async (req, res, next) => {
       .json({ message: "You are neither the course creator nor admin" });
   }
 };
+
+export const isEnrolled = async (req, res, next) => {
+  const { email, id } = req.decoded;
+  const { course_slug } = req.params;
+
+  // check if the user is the owner of this course
+  const course = await prisma.course.findUnique({
+    where: { slug: course_slug, userId: id },
+  });
+
+  if (course) {
+    return console.log("You are enrolled this course");
+  } else {
+    return console.log("You are not enrolled this course");
+  }
+  // check
+  const user = await prisma.user.findUnique({ where: { email } });
+
+  if (user.role === "admin" || course !== null) {
+    next();
+  } else {
+    res
+      .status(401)
+      .json({ message: "You are neither the course creator nor admin" });
+  }
+};
